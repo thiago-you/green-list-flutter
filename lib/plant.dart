ad8c3421533d319171ba0e28dba4b751f55769f3
thiago-you <thiago.youx@gmail.com>
2024-03-30 19:20:48 -0300

    implementing faq list

---
+++ lib/plant.dart
@@ -0,0 +1,40 @@
+class Post {
+  int? id;
+  String? name;
+  String? scientificName;
+  String? otherName;
+  String? cycle;
+  String? watering;
+  String? sunlight;
+  String? image;
+  String? thumbnail;
+
+  Post({
+    this.id,
+    this.name,
+    this.scientificName,
+    this.otherName,
+    this.cycle,
+    this.watering,
+    this.sunlight,
+    this.image,
+    this.thumbnail
+  });
+
+  Post.fromJson(Map<String, dynamic> json) {
+    id = json['title'];
+    name = json['common_name'];
+    scientificName = json['scientific_name'].isEmpty ? null : json['scientific_name'][0];
+    otherName = json['other_name'].isEmpty ? null : json['other_name'][0];
+    cycle = json['cycle'];
+    watering = json['watering'];
+    sunlight = json['sunlight'].isEmpty ? null : json['sunlight'][0];
+    image = json['default_image']?['original_url'] ?? "";
+    thumbnail = json['default_image']?['thumbnail'] ?? "";
+  }
+
+  @override
+  String toString() {
+    return 'Post{id: $id, name: $name, scientificName: $scientificName, otherName: $otherName, cycle: $cycle, watering: $watering, sunlight: $sunlight, image: $image, thumbnail: $thumbnail}';
+  }
+}
