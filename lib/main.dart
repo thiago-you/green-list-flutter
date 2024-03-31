f56d13d2f3eb02ee4c9974344444bb43ea0170a6
thiago-you <thiago.youx@gmail.com>
2024-03-30 22:06:09 -0300

    add pages navigation

--- lib/main.dart
+++ lib/main.dart
@@ -29,41 +29,31 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// faq page class
+class FaqPage extends StatefulWidget {
+  const FaqPage({super.key});
+
+  @override
+  State<FaqPage> createState() => _FaqPageState();
+}
+
+// plants page class
+class PlantsPage extends StatefulWidget {
+  const PlantsPage({super.key});
+
+  @override
+  State<PlantsPage> createState() => _PlantsPageState();
+}
+
 // homepage state
 class _MyHomePageState extends State<MyHomePage> {
-
-  // API key
-  static String? auth = "sk-B8oH6608468daafa24915";
-  static String? defaultThumbnail = "https://perenual.com/storage/article_faq/faq_13Y7nX6435fb7c71c37/medium.jpg";
-
-  // variable to call and store future list of plants
-  Future<List<Plant>> plantsFuture = getPlants();
-
-  // variable to call and store future list of faqs
-  Future<List<Faq>> faqsFuture = getFaqs();
-
-  // function to fetch data from api and return future list of plants
-  static Future<List<Plant>> getPlants() async {
-    var url = Uri.parse("https://perenual.com/api/species-list?key=$auth");
-    final response = await http.get(url, headers: {"Content-Type": "application/json"});
-    final List<dynamic> body = json.decode(response.body)["data"];
-    return body.map((e) => Plant.fromJson(e)).toList();
-  }
-
-  // function to fetch data from api and return future list of posts
-  static Future<List<Faq>> getFaqs() async {
-    var url = Uri.parse("https://perenual.com/api/article-faq-list?key=$auth");
-    final response = await http.get(url, headers: {"Content-Type": "application/json"});
-    final List<dynamic> body = json.decode(response.body)["data"];
-    return body.map((e) => Faq.fromJson(e)).toList();
-  }
 
   // build function
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Green Plant Guide'),
+        title: const Text('Green Life Guide'),
       ),
       body: Center(
         // FutureBuilder
@@ -72,7 +62,12 @@ class _MyHomePageState extends State<MyHomePage> {
             Expanded(
               child: Center (
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const FaqPage()),
+                    );
+                  },
                   style: ElevatedButton.styleFrom(
                       elevation: 12.0,
                       backgroundColor: Colors.green,
@@ -89,7 +84,12 @@ class _MyHomePageState extends State<MyHomePage> {
             Expanded(
               child: Center(
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const FaqPage()),
+                    );
+                  },
                   style: ElevatedButton.styleFrom(
                       elevation: 12.0,
                       backgroundColor: Colors.green,
@@ -103,24 +103,120 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               )
             )
-            // FutureBuilder<List<Faq>>(
-            //   future: faqsFuture,
-            //   builder: (context, snapshot) {
-            //     if (snapshot.connectionState == ConnectionState.waiting) {
-            //       // until data is fetched, show loader
-            //       return const CircularProgressIndicator();
-            //     } else if (snapshot.hasData) {
-            //       // once data is fetched, display it on screen (call buildFaqs())
-            //       final list = snapshot.data!;
-            //       return buildFaqs(list);
-            //       // return buildPlants(list);
-            //     } else {
-            //       // if no data, show simple Text
-            //       return const Text("No data available");
-            //     }
-            //   },
-            // ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _FaqPageState extends State<FaqPage> {
+
+  // API key
+  static String? auth = "sk-B8oH6608468daafa24915";
+  static String? defaultThumbnail = "https://perenual.com/storage/article_faq/faq_13Y7nX6435fb7c71c37/medium.jpg";
+
+  // variable to call and store future list of faqs
+  Future<List<Faq>> faqsFuture = getFaqs();
+
+  // function to fetch data from api and return future list of posts
+  static Future<List<Faq>> getFaqs() async {
+    var url = Uri.parse("https://perenual.com/api/article-faq-list?key=$auth");
+    final response = await http.get(url, headers: {"Content-Type": "application/json"});
+    final List<dynamic> body = json.decode(response.body)["data"];
+    return body.map((e) => Faq.fromJson(e)).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Green FAQ'),
+      ),
+      body: Center(
+        child: FutureBuilder<List<Faq>>(
+          future: faqsFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              // until data is fetched, show loader
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasData) {
+              // once data is fetched, display it on screen (call buildFaqs())
+              final list = snapshot.data!;
+              return buildFaqs(list);
+            } else {
+              // if no data, show simple Text
+              return const Text("No data available");
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  // function to display fetched data on screen
+  Widget buildFaqs(List<Faq> faqs) {
+    // ListView Builder to show data in a list
+    return ListView.builder(
+      itemCount: faqs.length,
+      itemBuilder: (context, index) {
+        final item = faqs[index];
+        return Container(
+          color: Colors.grey.shade300,
+          margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+          padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+          height: 100,
+          width: double.maxFinite,
+          child: Row(
+            children: [
+              Expanded(flex: 1, child: Image.network(item.thumbnail?.isEmpty == false ? item.thumbnail! : defaultThumbnail!)),
+              SizedBox(width: 10),
+              Expanded(flex: 3, child: Text(item.question!)),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _PlantsPageState extends State<PlantsPage> {
+
+  // API key
+  static String? auth = "sk-B8oH6608468daafa24915";
+
+  Future<List<Plant>> plantsFuture = getPlants();
+
+  // function to fetch data from api and return future list of plants
+  static Future<List<Plant>> getPlants() async {
+    var url = Uri.parse("https://perenual.com/api/species-list?key=$auth");
+    final response = await http.get(url, headers: {"Content-Type": "application/json"});
+    final List<dynamic> body = json.decode(response.body)["data"];
+    return body.map((e) => Plant.fromJson(e)).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plants List'),
+      ),
+      body: Center(
+        child: FutureBuilder<List<Plant>>(
+          future: plantsFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              // until data is fetched, show loader
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasData) {
+              // once data is fetched, display it on screen (call buildFaqs())
+              final list = snapshot.data!;
+              return buildPlants(list);
+            } else {
+              // if no data, show simple Text
+              return const Text("No data available");
+            }
+          },
         ),
       ),
     );
@@ -144,31 +240,6 @@ class _MyHomePageState extends State<MyHomePage> {
               Expanded(flex: 1, child: Image.network(item.thumbnail!)),
               SizedBox(width: 10),
               Expanded(flex: 3, child: Text(item.name!)),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  // function to display fetched data on screen
-  Widget buildFaqs(List<Faq> faqs) {
-    // ListView Builder to show data in a list
-    return ListView.builder(
-      itemCount: faqs.length,
-      itemBuilder: (context, index) {
-        final item = faqs[index];
-        return Container(
-          color: Colors.grey.shade300,
-          margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-          padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-          height: 100,
-          width: double.maxFinite,
-          child: Row(
-            children: [
-              Expanded(flex: 1, child: Image.network(item.thumbnail?.isEmpty == false ? item.thumbnail! : defaultThumbnail!)),
-              SizedBox(width: 10),
-              Expanded(flex: 3, child: Text(item.question!)),
             ],
           ),
         );
